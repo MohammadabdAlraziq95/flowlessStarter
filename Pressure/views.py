212219163fea4838f788fc16e265a91885fc3bf4
MohammadabdAlraziq95 <mohammadabdalraziq@gmail.com>
2022-07-24 13:48:00 +0300

    Week2

--- Pressure/views.py
+++ Pressure/views.py
@@ -1,5 +1,59 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework import  viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+
+from Pressure.models import PressureReading, PressureSensor
+from Pressure.serializer import PressureReadingSerializer, PressureSensorSerializer
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+
+
+
+
+# class PressureSensorViewSet(viewsets.ViewSet):
+#  queryset = PressureSensor.objects.all()
+   
+#  def list(self, request):
+#   serializer_class = PressureSensorSerializer(self.queryset, many=True)
+#   return Response(serializer_class.data)
+
+# def retrieve(self, request, pk=None):
+#     ps = get_object_or_404(self.queryset,pk=pk)
+#     serializer_class = PressureSensorSerializer(ps)
+#     return Response(serializer_class.data)
+ 
+# def list(self, request):
+#         pass
+
+# def create(self, request):
+#         pass
+
+# def retrieve(self, request, pk=None):
+#         pass
+
+# def update(self, request, pk=None):
+#         pass
+
+# def partial_update(self, request, pk=None):
+#         pass
+
+# def destroy(self, request, pk=None):
+#         pass
+
+class PressureSensorViewSet(viewsets.ModelViewSet):
+
+     queryset = PressureSensor.objects.all()
+     serializer_class = PressureSensorSerializer
+     filter_backends = [DjangoFilterBackend]
+     filterset_fields = ['ID', 'InstallationDate']
+
+
+class PressureReadingViewSet(viewsets.ModelViewSet):
+
+     queryset = PressureReading.objects.all()
+     serializer_class = PressureReadingSerializer
+     filter_backends = [DjangoFilterBackend]
+     filterset_fields = ['ID', 'DateTime']
--- flowlessStarter/urls.py
+++ flowlessStarter/urls.py
@@ -17,46 +17,14 @@ from pyexpat import model
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
-from django.contrib.auth.models import User
-from Pressure.models import PressureReading, PressureSensor
-from rest_framework import routers, serializers, viewsets
-from django_filters.rest_framework import DjangoFilterBackend
+from Pressure.views import PressureReadingViewSet, PressureSensorViewSet
+from rest_framework import routers
 
-
-class PressureSensorSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = PressureSensor
-        fields = ['ID', 'Label', 'InstallationDate', 'Latitude' , 'Longitude']
-
-
-class PressureReadingSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = PressureReading
-        fields = ['ID', 'DateTime', 'Value', 'SensorId']
-
-class PressureSensorViewSet(viewsets.ModelViewSet):
-
-     queryset = PressureSensor.objects.all()
-     serializer_class = PressureSensorSerializer
-     filter_backends = [DjangoFilterBackend]
-     filterset_fields = ['ID', 'InstallationDate']
-
-class PressureReadingViewSet(viewsets.ModelViewSet):
-
-     queryset = PressureReading.objects.all()
-     serializer_class = PressureReadingSerializer
-     filter_backends = [DjangoFilterBackend]
-     filterset_fields = ['ID', 'DateTime']
-# router 
 router = routers.DefaultRouter()
 router.register(r'ps', PressureSensorViewSet)
 router.register(r'pr', PressureReadingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Pressure/', include('Pressure.urls')),
     path('api/', include(router.urls)),
-    path('rest/', include('rest_framework.urls', namespace='rest_framework'))
-
-
 ]
