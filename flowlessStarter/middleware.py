8bf56485e5b3cab9367e0bdceda2529a35f84833
MohammadabdAlraziq95 <mohammadabdalraziq@gmail.com>
2022-08-19 14:53:15 +0300

    changes

--- flowlessStarter/middleware.py
+++ flowlessStarter/middleware.py
@@ -1,10 +1,14 @@
 
+import time
+from urllib import response
+
+
 def Timing(get_response):
 
     def middleware(request):
         start_time = time.time()
         response = get_response(request)
         duration = time.perf_counter() / 1000000
-        print("took " + f'{duration:.3f} ' +  " seconds to complete") 
+        print("the request " + request.path + " took " + f'{duration:.3f} ' +  " seconds to complete") 
         return response
     return middleware
